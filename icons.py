a64f5dc3d7d2f12658b9b633e33323a358027cf6
Sze Wong <swong@zerionsoftware.com>
2017-08-12 18:27:09 -0400

    .

--- icons.py
+++ icons.py
@@ -4,12 +4,12 @@ b = (0,0,255)
 d = (0,0,0)
 
 wifi = [
-d,d,d,d,d,d,d,d
-d,d,r,r,r,r,d,d
-d,r,d,d,d,d,r,d
-r,d,d,d,d,d,d,r
-d,d,d,r,r,d,d,d
-d,d,r,d,d,r,d,d
-d,d,d,d,d,d,d,d
+d,d,d,d,d,d,d,d,
+d,d,r,r,r,r,d,d,
+d,r,d,d,d,d,r,d,
+r,d,d,d,d,d,d,r,
+d,d,d,r,r,d,d,d,
+d,d,r,d,d,r,d,d,
+d,d,d,d,d,d,d,d,
 d,d,d,r,r,d,d,d
 ]
